0992c267c72a0fe15494907d9c4fc37672d3e3a5
jitendra1404 <sahjitendra731@gmail.com>
2021-01-29 04:40:47 +0545

    Intent apply to coordinate with user and register

--- app/src/main/java/com/example/androidproject25b/LoginActivity.kt
+++ app/src/main/java/com/example/androidproject25b/LoginActivity.kt
@@ -1,11 +1,40 @@
 package com.example.androidproject25b
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etUsername :EditText
+    private lateinit var etPassword :EditText
+    private lateinit var etsignUp :TextView
+    private lateinit var btnLogin :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etUsername =findViewById(R.id.edUserName)
+        etPassword =findViewById(R.id.edloginpassword)
+        etsignUp =findViewById(R.id.txt_sign_up)
+        btnLogin =findViewById(R.id.btnSingIn)
+        etUsername.setText("")
+        etPassword.setText("")
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+        etsignUp.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
+        }
+    }
+
+    private fun login(){
+        val U_name = etUsername.text.toString()
+        val U_passworc =etPassword.text.toString()
+
     }
 }
