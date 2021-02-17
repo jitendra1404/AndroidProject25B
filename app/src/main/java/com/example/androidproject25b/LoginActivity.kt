1c5b8a7f847adcb09b7a515649357683d2ad5b5c
jitendra1404 <sahjitendra731@gmail.com>
2021-02-17 23:26:07 +0545

    create user repository

--- app/src/main/java/com/example/androidproject25b/LoginActivity.kt
+++ app/src/main/java/com/example/androidproject25b/LoginActivity.kt
@@ -2,26 +2,38 @@ package com.example.androidproject25b
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.core.app.ActivityCompat
 import com.example.androidproject25b.Entity.User
+import com.example.androidproject25b.Repository.UserRepository
+import com.example.androidproject25b.api.ServiceBuilder
 import com.example.androidproject25b.db.UserDB
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
+
+
+    private val permissions = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
 
     private lateinit var etUsername :EditText
     private lateinit var etPassword :EditText
     private lateinit var etsignUp :TextView
     private lateinit var btnLogin :Button
-
+    private lateinit var checkRememberMe:CheckBox
+    private lateinit var linearLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +43,13 @@ class LoginActivity : AppCompatActivity() {
         etPassword =findViewById(R.id.edloginpassword)
         etsignUp =findViewById(R.id.txt_sign_up)
         btnLogin =findViewById(R.id.btnSingIn)
+        checkRememberMe=findViewById(R.id.checkBox)
+        linearLayout=findViewById(R.id.linearLayout)
+
         etUsername.setText("")
         etPassword.setText("")
 
+        checkRunTimePermission()
         btnLogin.setOnClickListener {
 
             saveSharePref()
@@ -48,6 +64,30 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun checkRunTimePermission() {
+        if (!hasPermission()) {
+            requestPermission()
+        }
+    }
+    private fun hasPermission():Boolean {
+        var hasPermission=true
+        for (permission in permissions){
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                !=PackageManager.PERMISSION_GRANTED) {
+                hasPermission=false
+                break
+            }
+        }
+        return hasPermission
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@LoginActivity, permissions, 1
+        )
+    }
+
     private fun saveSharePref(){
         val Username =etUsername.text.toString()
         val Password =etPassword.text.toString()
@@ -72,21 +112,47 @@ class LoginActivity : AppCompatActivity() {
 
         var user:User?=null
         CoroutineScope(Dispatchers.IO).launch {
-            user =UserDB.getInstance(this@LoginActivity)
-                .getUserDAO().checkUser(U_name, U_password)
+            try {
+                val repository=UserRepository()
+                val response=repository.loginUser(U_name, U_password)
+                if (response.success==true){
+                    ServiceBuilder.token= "Bearer " + response.token
 
-            if (user==null) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(this@LoginActivity, "Invalid credentials", Toast.LENGTH_SHORT)
-                        .show()
+                    startActivity(Intent(this@LoginActivity,
+                        DashboardActivity::class.java))
+                    finish()
+                } else{
+                    withContext(Dispatchers.Main){
+                        val snack = Snackbar.make(linearLayout,
+                            "Invalid Credentials", Snackbar.LENGTH_LONG)
+                        snack.setAction("Ok", View.OnClickListener {
+                            snack.dismiss()
+                        })
+                        snack.show()
+                    }
                 }
-            }else {
-
-                val intent=(Intent(this@LoginActivity, DashboardActivity::class.java))
-                startActivity(intent)
+            }catch (ex:Exception){
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@LoginActivity, "Login Fail", Toast.LENGTH_SHORT).show()
+                }
+//            }
+//            user =UserDB.getInstance(this@LoginActivity)
+//                .getUserDAO().checkUser(U_name, U_password)
+//
+//            if (user==null) {
+//                withContext(Dispatchers.Main) {
+//                    Toast.makeText(this@LoginActivity, "Invalid credentials", Toast.LENGTH_SHORT)
+//                        .show()
+//                }
+//            }else {
+//
+//                val intent=(Intent(this@LoginActivity, DashboardActivity::class.java))
+//                startActivity(intent)
+//            }
             }
 
         }
 
     }
+
 }
