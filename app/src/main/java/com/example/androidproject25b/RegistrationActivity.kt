e1829bf30995405b037ea94571d82c70c3fa5cc5
jitendra1404 <sahjitendra731@gmail.com>
2021-02-18 00:20:03 +0545

    implement loginresponse class

--- app/src/main/java/com/example/androidproject25b/RegistrationActivity.kt
+++ app/src/main/java/com/example/androidproject25b/RegistrationActivity.kt
@@ -7,10 +7,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.androidproject25b.Entity.User
+import com.example.androidproject25b.Repository.UserRepository
+import com.example.androidproject25b.api.ServiceBuilder
 import com.example.androidproject25b.db.UserDB
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class RegistrationActivity : AppCompatActivity() {
 
@@ -49,7 +54,25 @@ class RegistrationActivity : AppCompatActivity() {
 
             }else {
                 val user =User(username= username, address = address, mobile = mobile, email = email, password = Password)
+                Toast.makeText(this, "AAYO KALO KETA", Toast.LENGTH_SHORT).show()
                 CoroutineScope(Dispatchers.IO).launch {
+
+                    val repository=UserRepository()
+                    try {
+                        val response= repository.registerUser(user)
+                        if (response.success==true){
+                            ServiceBuilder.token == "Bearer " +response.token
+                            withContext(Main){
+                                Toast.makeText(this@RegistrationActivity, "Successfully registered", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }catch (ex:Exception){
+                        withContext(Main){
+                            Toast.makeText(this@RegistrationActivity, "Register Fail", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+
                     UserDB.getInstance(this@RegistrationActivity).getUserDAO().resgisterUser(user)
                 }
                 Toast.makeText(this, "User sinUp", Toast.LENGTH_SHORT).show()
