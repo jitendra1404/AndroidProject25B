b385de46bbe2d491886844d4dd8248a26e9ed84b
jitendra1404 <sahjitendra731@gmail.com>
2021-03-01 14:09:24 +0545

    implement googleMap in about Fragment

---
+++ app/src/main/java/com/example/androidproject25b/AddAppointmentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidproject25b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddAppointmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_appointment)
+    }
+}
---
+++ app/src/main/java/com/example/androidproject25b/adapter/AppointmentAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.androidproject25b.adapter
+
+class AppointmentAdapter {
+}
