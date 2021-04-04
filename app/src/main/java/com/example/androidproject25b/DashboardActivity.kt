1c1af27096348257a1fe7c2c4d239bcd475e15f3
jitendra1404 <sahjitendra731@gmail.com>
2021-04-04 09:19:17 +0545

    setup navigationdraweractivity

--- app/src/main/java/com/example/androidproject25b/DashboardActivity.kt
+++ app/src/main/java/com/example/androidproject25b/DashboardActivity.kt
@@ -8,42 +8,17 @@ import android.os.PersistableBundle
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.example.androidproject25b.Fragments.AboutFragment
-import com.example.androidproject25b.Fragments.AppointmentFragment
-import com.example.androidproject25b.Fragments.DashboardFragment
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
-
-  private val dashboardFragment=DashboardFragment()
-    private val appointmentFragment=AppointmentFragment()
-    private val aboutFragment = AboutFragment()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        replaceFragment(dashboardFragment)
-
-        buttom_navigation.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.DashboardPage -> replaceFragment(dashboardFragment)
-                R.id.AppointmentPage -> replaceFragment(appointmentFragment)
-                R.id.AboutPage -> replaceFragment(aboutFragment)
-            }
-            true
-        }
-
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        if (fragment!=null){
-            val transaction=supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragment)
-            transaction.commit()
-        }
 
     }
 
