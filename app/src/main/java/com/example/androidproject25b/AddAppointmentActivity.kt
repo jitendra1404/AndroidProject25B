794dd2ceba940c5001d32b4d2a0a424893d93056
jitendra1404 <sahjitendra731@gmail.com>
2021-04-14 23:53:07 +0545

    change in appointment activity

--- app/src/main/java/com/example/androidproject25b/AddAppointmentActivity.kt
+++ app/src/main/java/com/example/androidproject25b/AddAppointmentActivity.kt
@@ -7,5 +7,7 @@ class AddAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_appointment)
+
+        // this is appointment activity//
     }
 }
