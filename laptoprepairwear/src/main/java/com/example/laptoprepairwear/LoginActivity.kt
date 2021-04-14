ffa5d8596fe5cd7576c6716a9c47f9a4f83c8b44
jitendra1404 <sahjitendra731@gmail.com>
2021-04-14 21:29:31 +0545

    implement wearod for laptoprepair project

--- laptoprepairwear/src/main/java/com/example/laptoprepairwear/LoginActivity.kt
+++ laptoprepairwear/src/main/java/com/example/laptoprepairwear/LoginActivity.kt
@@ -6,6 +6,8 @@ import android.support.wearable.activity.WearableActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.laptoprepairwear.Repository.UserRepository
+import com.example.laptoprepairwear.api.ServiceBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,11 +20,10 @@ class LoginActivity : WearableActivity() {
     private lateinit var etPassword: EditText
     private lateinit var btnLogin: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        // Enables Always-on
-        setAmbientEnabled()
 
         etUsername = findViewById(R.id.edUserName)
         etPassword = findViewById(R.id.edloginpassword)
@@ -32,39 +33,41 @@ class LoginActivity : WearableActivity() {
             login()
 
         }
+        // Enables Always-on
+        setAmbientEnabled()
 
     }
 
     private fun login() {
-//        val U_name = etUsername.text.toString()
-//        val U_password = etPassword.text.toString()
-//
-////        var user: User? = null
-//        CoroutineScope(Dispatchers.IO).launch {
-//
-//            try {
-//                val repository = UserRepository()
-//                val response = repository.loginUser(U_name, U_password)
-//                if (response.success == true) {
-//                    ServiceBuilder.token = "Bearer " + response.token
-//
-//                    startActivity(
-//                        Intent(
-//                            this@LoginActivity,
-//                            DashboardActivity::class.java
-//                        )
-//                    )
-//                    finish()
-//                }
-//            } catch (ex: Exception) {
-//                withContext(Dispatchers.Main) {
-//                    Toast.makeText(
-//                        this@LoginActivity,
-//                        ex.toString(), Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//
-//            }
-//        }
+        val U_name = etUsername.text.toString()
+        val U_password = etPassword.text.toString()
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            try {
+                val repository = UserRepository()
+                val response = repository.loginUser(U_name, U_password)
+                if (response.success == true)
+                withContext(Dispatchers.Main){
+                    ServiceBuilder.token = "Bearer " + response.token
+
+                    startActivity(
+                        Intent(
+                            this@LoginActivity,
+                            DashboardActivity::class.java
+                        )
+                    )
+                    finish()
+                }
+            } catch (ex: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        ex.toString(), Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+        }
     }
 }
